684c869a9e8cc787444d770272957136ddaa7e1f
arihant-jain0 <jainari1208@gmail.com>
2024-03-30 15:08:34 +0530

    update code

--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI!);
-  } catch (error) {
-    console.log("Error: ", error);
+    const connection = await mongoose.connect(process.env.MONGODB_URI!, {
+    
+    });
+    console.log("Connected to MongoDB 😃😃😃😃");
+  } catch (error:any) {
+    console.error("Error connecting to MongoDB:", error.message);
+    // Optionally, you could re-throw the error to propagate it further
+    throw error;
   }
 };
 
