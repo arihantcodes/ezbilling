004e8840fcb70aded97ffea05cb4ccb4873a806d
arihant-jain0 <jainari1208@gmail.com>
2024-04-05 18:32:30 +0530

    Fix bug in login functionality

--- app/apps/[id]/page.tsx
+++ app/apps/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import {
   Bell,
@@ -9,7 +9,6 @@ import {
   Package,
   Package2,
   Search,
-
   Users,
   Settings,
 } from "lucide-react";
@@ -45,8 +44,8 @@ import { useRouter } from "next/navigation";
 
 import { useState } from "react";
 
-export default function Dashboard({params}:any) {
-   const router = useRouter();
+export default function Dashboard({ params }: any) {
+  const router = useRouter();
   const logout = async () => {
     try {
       await axios.get("/api/user/logout");
@@ -58,7 +57,7 @@ export default function Dashboard({params}:any) {
     }
   };
   const [formData, setFormData] = useState({
-    userId: "",
+    userId: params.id,
     billname: "",
     billno: "",
     billdate: "",
@@ -132,21 +131,21 @@ export default function Dashboard({params}:any) {
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
-                 href={`/apps/${params.id}`}
+                href={`/apps/${params.id}`}
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Home className="h-4 w-4" />
                 Dashboard
               </Link>
               <Link
-                 href={`/apps/settings/${params.id}`}
+                href={`/apps/settings/${params.id}`}
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Settings className="h-4 w-4" />
                 Settings
               </Link>
               <Link
-                 href={`/apps/yourbill/${params.id}`}
+                href={`/apps/yourbill/${params.id}`}
                 className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
               >
                 <Package className="h-4 w-4" />
@@ -223,21 +222,22 @@ export default function Dashboard({params}:any) {
                   />
                   <span className="">EzBilling</span>
                 </Link>
-                <Link href={`/apps/${params.id}`}
+                <Link
+                  href={`/apps/${params.id}`}
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
                   <Home className="h-5 w-5" />
                   Dashboard
                 </Link>
                 <Link
-               href={`/apps/settings/${params.id}`}
+                  href={`/apps/settings/${params.id}`}
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
                 >
                   <Settings className="h-5 w-5" />
                   Settings
                 </Link>
                 <Link
-               href={`/apps/settings/${params.id}`}
+                  href={`/apps/settings/${params.id}`}
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
                   <Package className="h-5 w-5" />
@@ -302,151 +302,151 @@ export default function Dashboard({params}:any) {
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem>
-                <Link href={`/profile/${params.id}`}>
-                Profile
-                </Link>
+                <Link href={`/profile/${params.id}`}>Profile</Link>
               </DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
               <div>
-              <DropdownMenuItem onClick={logout}>Logout
-              </DropdownMenuItem>
-              <Toaster position="top-right" reverseOrder={false} />
+                <DropdownMenuItem onClick={logout}>Logout</DropdownMenuItem>
+                <Toaster position="top-right" reverseOrder={false} />
               </div>
             </DropdownMenuContent>
           </DropdownMenu>
           <ModeToggle />
         </header>
         <main className="flex  dark:bg-black  flex-row flex-wrap justify-evenly gap-4 p-4 lg:gap-6 lg:p-6">
-        <div className="md:w-[800px] text-center flex flex-col justify-center items-center">
-        <h1 className="font-bold md:text-4xl">Create Your Bill in 3 steps</h1>
-        <div className="grid md:grid-cols-2 gap-6 mt-12 min-h-[60px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-500 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 text-start">
-          <Input
-            type="text"
-            placeholder="Bill Name"
-            name="billname"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="text"
-            placeholder="Bill No."
-            name="billno"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="date"
-            placeholder="Bill Date"
-            name="billdate"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="text"
-            placeholder="User ID"
-            name="userId"
-            className="w-60"
-            onChange={handleInputChange}
-            
-           
-          />
-          <div className="">
-            <h1 className="font-bold text-lg ">Your Details</h1>
-            <Input
-              type="text"
-              placeholder="Business Name"
-              name="businessname"
-              className="w-60 mb-5 mt-3"
-              onChange={handleInputChange}
-            />
-            <Input
-              type="text"
-              placeholder="Business Address"
-              name="businessaddress"
-              className="w-60 mb-5"
-              onChange={handleInputChange}
-            />
-            <Input
-              type="text"
-              placeholder="Business GSTIN (Optional)"
-              name="gstno"
-              className="w-60 mb-5"
-              onChange={handleInputChange}
-            />
-            <Input
-              type="text"
-              placeholder="Business PAN Number (Optional)"
-              name="panno"
-              className="w-60 mb-5"
-              onChange={handleInputChange}
-            />
+          <div className="md:w-[800px] text-center flex flex-col justify-center items-center">
+            <h1 className="font-bold md:text-4xl">
+              Create Your Bill in 3 steps
+            </h1>
+            <div className="grid md:grid-cols-2 gap-6 mt-12 min-h-[60px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-500 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 text-start">
+              <Input
+                type="text"
+                placeholder="Bill Name"
+                name="billname"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="text"
+                placeholder="Bill No."
+                name="billno"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="date"
+                placeholder="Bill Date"
+                name="billdate"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="text"
+                placeholder="User ID"
+                name="userId"
+                className="w-60"
+                onChange={handleInputChange}
+                value={formData.userId}
+                readOnly
+              />
+
+              <div className="">
+                <h1 className="font-bold text-lg ">Your Details</h1>
+                <Input
+                  type="text"
+                  placeholder="Business Name"
+                  name="businessname"
+                  className="w-60 mb-5 mt-3"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Business Address"
+                  name="businessaddress"
+                  className="w-60 mb-5"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Business GSTIN (Optional)"
+                  name="gstno"
+                  className="w-60 mb-5"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Business PAN Number (Optional)"
+                  name="panno"
+                  className="w-60 mb-5"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <h1 className="font-bold text-lg ">Client Details</h1>
+                <Input
+                  type="text"
+                  placeholder="Client Name"
+                  name="clientname"
+                  className="w-60 mb-7 mt-3"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Client Address"
+                  name="clientaddress"
+                  className="w-60 mb-7"
+                  onChange={handleInputChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Client Mobile No."
+                  name="clientmobileno"
+                  className="w-60 mb-7"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <Input
+                type="text"
+                placeholder="Item Name"
+                name="itemname"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="number"
+                placeholder="Quantity"
+                name="itemquantity"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="number"
+                placeholder="Price"
+                name="itemprice"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="number"
+                placeholder=" Item Total"
+                name="itemtotal"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="number"
+                placeholder="Total Amount"
+                name="totalamount"
+                className="w-60"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="flex mt-5 items-end">
+              <Button onClick={billing}>Save & Continue</Button>
+              <Toaster position="top-right" reverseOrder={false} />
+            </div>
           </div>
-          <div>
-            <h1 className="font-bold text-lg ">Client Details</h1>
-            <Input
-              type="text"
-              placeholder="Client Name"
-              name="clientname"
-              className="w-60 mb-7 mt-3"
-              onChange={handleInputChange}
-            />
-            <Input
-              type="text"
-              placeholder="Client Address"
-              name="clientaddress"
-              className="w-60 mb-7"
-              onChange={handleInputChange}
-            />
-            <Input
-              type="text"
-              placeholder="Client Mobile No."
-              name="clientmobileno"
-              className="w-60 mb-7"
-              onChange={handleInputChange}
-            />
-          </div>
-          <Input
-            type="text"
-            placeholder="Item Name"
-            name="itemname"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            placeholder="Quantity"
-            name="itemquantity"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            placeholder="Price"
-            name="itemprice"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            placeholder=" Item Total"
-            name="itemtotal"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            placeholder="Total Amount"
-            name="totalamount"
-            className="w-60"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex mt-5 items-end">
-          <Button onClick={billing}>Save & Continue</Button>
-          <Toaster position="top-right" reverseOrder={false} />
-        </div>
-      </div>
         </main>
       </div>
     </div>
